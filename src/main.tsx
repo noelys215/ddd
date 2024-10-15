246936a061ef49487a0cd058448b6adb4a545d1a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-10-15 19:58:47 -0400

    add router dom and routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,21 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Home } from './views/Home.tsx';
 import { Experience } from './views/Experience.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/experience',
+		element: <Experience />,
+	},
+]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Home />
-		{/* <Experience /> */}
+		<RouterProvider router={router} />
 	</StrictMode>
 );
