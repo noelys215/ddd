c1a3c63a1c3885c5950325db77c3de91df4d8acc
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-10-23 20:49:28 -0400

    feat: replace swiper with enbla

--- src/components/Silder.tsx
+++ src/components/Silder.tsx
@@ -1,48 +1,53 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useCallback } from 'react';
+import Fade from 'embla-carousel-fade';
+import useEmblaCarousel from 'embla-carousel-react';
+import { EmblaOptionsType } from 'embla-carousel';
+import { CaretLeft, CaretRight } from '@phosphor-icons/react';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
-import { EffectFade, Navigation, Pagination } from 'swiper/modules';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
 interface SliderProps {
 	array: { src: string; alt: string }[];
+	options?: EmblaOptionsType;
 }
 
 export const Slider: React.FC<SliderProps> = ({ array }) => {
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, duration: 30 }, [Fade()]);
+
+	const scrollPrev = useCallback(() => {
+		if (emblaApi) emblaApi.scrollPrev();
+	}, [emblaApi]);
+
+	const scrollNext = useCallback(() => {
+		if (emblaApi) emblaApi.scrollNext();
+	}, [emblaApi]);
+
 	return (
-		<Swiper
-			className="swiper"
-			keyboard={{
-				enabled: true,
-			}}
-			effect="fade"
-			slidesPerView={1}
-			spaceBetween={30}
-			loop={true}
-			pagination={{
-				clickable: true,
-			}}
-			navigation={true}
-			modules={[Pagination, EffectFade, Navigation]}>
-			{array.map((item) => (
-				<SwiperSlide className="swiper-slide" key={item.src}>
-					<Zoom>
-						<div className="relative w-full h-0 pb-[56.25%]">
-							<img
-								src={item.src}
-								alt={item.alt}
-								className="absolute top-0 left-0 w-full h-0  rounded-lg"
-							/>
+		<div className="slider-container w-full mx-auto relative">
+			<div className="embla" ref={emblaRef}>
+				<div className="embla__container">
+					{array.map((item) => (
+						<div className="embla__slide relative" key={item.src}>
+							<Zoom>
+								<img
+									src={item.src}
+									alt={item.alt}
+									className="w-full h-auto object-contain rounded-lg"
+								/>
+							</Zoom>
 						</div>
-					</Zoom>
-				</SwiperSlide>
-			))}
-		</Swiper>
+					))}
+				</div>
+				{/* Arrows */}
+				<div className="arrow-container absolute inset-0 flex justify-between items-center pointer-events-none">
+					<button className="embla__prev pointer-events-auto" onClick={scrollPrev}>
+						<CaretLeft size={40} weight="bold" className="text-pink-500" />
+					</button>
+					<button className="embla__next pointer-events-auto" onClick={scrollNext}>
+						<CaretRight size={40} weight="bold" className="text-pink-500" />
+					</button>
+				</div>
+			</div>
+		</div>
 	);
 };
