0067fd696c3f072a7bec2d4c193efc5675a17fdc
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-11-06 21:03:29 -0500

    chore: fun css lolz

--- src/components/BioCard.tsx
+++ src/components/BioCard.tsx
@@ -182,13 +182,13 @@ const BioCard: React.FC<BioCardProps> = ({
 			{/* Horizontal Line */}
 			<div className="relative mb-4">
 				<hr className="border-gray-400 w-4/5 mx-auto" aria-hidden="true" />
-				<div className="absolute inset-x-0 top-0 flex justify-center -mt-6">
+				<div className="absolute inset-x-0 top-0 flex justify-center -mt-5">
 					<Rabbit
 						fill="#FFF"
-						// fill="#FF69B4"
-						size={32}
+						size={34}
 						weight="fill"
 						onClick={() => navigate('/novella/calling')}
+						className="rotate"
 					/>
 				</div>
 			</div>
@@ -211,15 +211,6 @@ const BioCard: React.FC<BioCardProps> = ({
 					Experience
 				</button>
 			</footer>
-			{/* Rabbit Icon fixed at bottom left */}
-			{/* <div className="fixed bottom-0 left-0 md:bottom-4 md:left-4">
-				<Rabbit
-					size={25}
-					weight="duotone"
-					color="#fff"
-					onClick={() => navigate('/novella/calling')}
-				/>
-			</div> */}
 		</article>
 	);
 };
