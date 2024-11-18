ec3c066527561075de1a3a70e2860a8538d5ecb9
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-11-18 18:56:19 -0500

    chore: update bio

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -16,10 +16,12 @@ export const Home: React.FC = () => {
 					linkedinUrl="https://www.linkedin.com/in/henry-betancourth/"
 					text="Hi there! I’m a self-motivated full-stack software engineer based in Southern New Jersey.         
         I take great pleasure in designing websites that are not only visually appealing but also easy to use. I’m all about simplicity in both design and code.
-        While JavaScript is my go-to language, with a strong grasp of React and Next.js, I consider myself language-agnostic and am comfortable working with any programming language. I doubt your actually reading all this so click on the spinning rabbit.  
+        While JavaScript is my go-to language, with a strong grasp of React and Next.js, I consider myself language-agnostic and am comfortable working with any programming language.   
 				I’m always on the lookout for new technologies to master, and staying current while expanding my skill set is something I’m passionate about.
         During college, I had the opportunity to learn and work with AWS and Java/Spring Boot, further broadening my expertise in backend development and cloud services.
-        I invite you to click around and explore my website to see the projects and work that I’m most proud of. I hope you’ll find the creativity and precision in my work as exciting as I do."
+        I invite you to click around and explore my website to see the projects and work that I’m most proud of. I hope you’ll find the creativity and precision in my work as exciting as I do.
+				[ hidden areas of this website is still under construction, explore at you discretion ] 
+				"
 				/>
 			</MotionSection>
 		</Layout>
