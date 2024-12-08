7f14768bf9648151ce36a6977658b8c60380e752
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-07 20:33:51 -0500

    chore: update bionic text css

--- src/hooks/setBionicReading.tsx
+++ src/hooks/setBionicReading.tsx
@@ -1,10 +1,10 @@
 export const setBionicReading = (text: string): string => {
 	const transformToBionic = (word: string): string => {
 		const splitIndex = Math.ceil(word.length / 2); // Bold the first half
-		return `<b style="font-weight: 700; color: #ffffff;">${word.slice(
+		return `<strong style="font-weight: 700; color: #ffffff;">${word.slice(
 			0,
 			splitIndex
-		)}</b><span style="color: rgba(255, 255, 255, 0.8);">${word.slice(splitIndex)}</span>`;
+		)}</strong><span style="color: rgba(255, 255, 255, 0.8);">${word.slice(splitIndex)}</span>`;
 	};
 	return text.split(' ').map(transformToBionic).join(' ');
 };
