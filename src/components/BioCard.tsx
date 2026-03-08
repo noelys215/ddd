886e7832bb37d4b053236718ce27e3c27b46fae9
Henry Betancourth <noelys215@gmail.com>
2026-03-07 22:32:54 -0500

    feat: gitch effect changes

--- src/components/BioCard.tsx
+++ src/components/BioCard.tsx
@@ -131,7 +131,7 @@ const BioCard: React.FC<BioCardProps> = ({
     step: 1,
     scramble: 5,
     seed: 4,
-    range: [8700, 10300],
+    range: [8704, 8959],
     playOnMount: true,
     onAnimationEnd: () => setIsInitialNameScrambleDone(true),
   });
@@ -144,7 +144,7 @@ const BioCard: React.FC<BioCardProps> = ({
       step: 1,
       scramble: 5,
       seed: 4,
-      range: [8700, 10300],
+      range: [8704, 8959],
       playOnMount: false,
       overdrive: true,
     });
