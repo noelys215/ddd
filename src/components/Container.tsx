95ddd66905d4bdf7e8c13ff1e9b35a0d3d2fcfb3
Henry Betancourth <noelys215@gmail.com>
2026-03-08 01:09:55 -0500

    feat: border changes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,18 +1,23 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 interface LayoutProps {
-	children: ReactNode;
-	color?: string;
-	opacity?: number;
+  children: ReactNode;
+  color?: string;
+  opacity?: number;
 }
 
-export const Container: React.FC<LayoutProps> = ({ children, color = '#101010', opacity = 95 }) => {
-	return (
-		<article
-			className={`border border-white rounded-lg max-w-4xl w-full p-6 md:p-12 bg-black shadow-md mx-auto opacity-${opacity}`}
-			style={{ backgroundColor: color }}
-			aria-labelledby="card-title">
-			{children}
-		</article>
-	);
+export const Container: React.FC<LayoutProps> = ({
+  children,
+  color = "#101010",
+  opacity = 95,
+}) => {
+  return (
+    <article
+      className={`border border-white rounded-lg max-w-4xl w-full p-6 md:p-12 bg-black shadow-md mx-auto opacity-${opacity}`}
+      style={{ backgroundColor: color }}
+      aria-labelledby="card-title"
+    >
+      {children}
+    </article>
+  );
 };
